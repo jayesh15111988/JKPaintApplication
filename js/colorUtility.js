6b0f6c6fb3be6c5f738b9b01fe2128f84b77cefe
Jayesh Kawli <j.kawli@gmail.com>
2015-03-01 19:22:47 -0500

    Some minor additions to show selected color on viewport

--- js/colorUtility.js
+++ js/colorUtility.js
@@ -3,12 +3,13 @@
 
 function isThatColor() {
 
-	var color = new RGBColor($('#selColorValue').value);
+
+	var color = new RGBColor($('#selColorValue')[0].value);
 	if (color.ok) {
-		$('#result1').style.backgroundColor = 'rgb(' + color.r + ', ' + color.g + ', ' + color.b + ')';
-		$('#result-text').innerHTML = 'Red: ' + color.r + '<br />Green: ' + color.g + '<br />Blue: ' + color.b + '<br />RGB: ' + color.toRGB() + '<br />Hex: ' + color.toHex();
+		$('#result1')[0].style.backgroundColor = 'rgb(' + color.r + ', ' + color.g + ', ' + color.b + ')';
+		$('#result-text')[0].innerHTML = 'Red: ' + color.r + '<br />Green: ' + color.g + '<br />Blue: ' + color.b + '<br />RGB: ' + color.toRGB() + '<br />Hex: ' + color.toHex();
 	} else {
-		$('#result-text').innerHTML = 'Never heard of such color :(';
-		$('#result').style.backgroundColor = 'rgb(255, 255, 255)';
+		$('#result1')[0].style.backgroundColor = 'rgb(255, 255, 255)';
+		$('#result-text')[0].innerHTML = 'Never heard of such color :(';
 	}
 }
